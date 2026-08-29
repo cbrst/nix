d4934a7cf9e7a643880a47c82206acdc0245f86e
Christian Brassat <christian@brassat.eu>
2026-08-29 11:56:09 +0200

    chore: remove redundant Neovim option comments
    Delete generic guidance comments from the Neovim options configuration, leaving
    the functional settings unchanged and keeping the file focused on active options.

--- modules/home/neovim/configs/lua/options.lua
+++ modules/home/neovim/configs/lua/options.lua
@@ -2,8 +2,6 @@ local icons = require("utils.icons")
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
 
 -- Make line numbers default
 vim.opt.number = true
